f8f559248b132cea5493df7391247d3639af0c37
mkfsn <p408865@gmail.com>
2017-06-19 22:39:49 +0800

    Update channel and program model

--- restserver/model/channel.go
+++ restserver/model/channel.go
@@ -1,7 +1,14 @@
 package model
 
+import (
+	"time"
+)
+
 type Channel struct {
-	ID          int    `gorm:"AUTO_INCREMENT"`
-	Name        string `gorm:"not null"`
-	Destription string `gorm:"not null"`
+	ID          int       `gorm:"primary_key;not null;AUTO_INCREMENT"`
+	Name        string    `gorm:"not null"`
+	Owner       string    `gorm:"not null"`
+	Destription string    `gorm:"not null"`
+	CreatedAt   time.Time `gorm:"not null"`
+	ModifiedAt  time.Time `gorm:"not null"`
 }
--- restserver/model/program.go
+++ restserver/model/program.go
@@ -5,18 +5,13 @@ import (
 )
 
 type Program struct {
-	//      CREATE TABLE IF NOT EXISTS `programtemplate` (
-	ID      int       `gorm:"primary_key;not null;AUTO_INCREMENT" form:"id"`
-	Day     int       `gorm:"not null" form:"day"`
-	Title   string    `gorm:"not null" form:"title"`
-	Content string    `gorm:"not null" form:"content"`
-	Link    string    `gorm:"not null" form:"link"`
-	Flag    int       `gorm:"not null" form:"flag"`
-	Start   time.Time `gorm:"not null" form:"start"`
-	End     time.Time `gorm:"not null" form:"end"`
-	Create  time.Time `gorm:"not null" form:"create"`
-	// `modify` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-	Modify time.Time `gorm:"not null" form:"modify"`
-
-	// ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+	ID         int       `gorm:"primary_key;not null;AUTO_INCREMENT"`
+	Day        int       `gorm:"not null"`
+	Title      string    `gorm:"not null"`
+	Content    string    `gorm:"not null"`
+	Link       string    `gorm:"not null"`
+	StartedAt  time.Time `gorm:"not null"`
+	EndedAt    time.Time `gorm:"not null"`
+	CreatedAt  time.Time `gorm:"not null"`
+	ModifiedAt time.Time `gorm:"not null"`
 }
