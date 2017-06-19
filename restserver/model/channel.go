cea61c6eebc13a900ea49bc777ab86aca65d0533
mkfsn <p408865@gmail.com>
2017-06-20 00:55:20 +0800

    Typo

--- restserver/model/channel.go
+++ restserver/model/channel.go
@@ -8,7 +8,7 @@ type Channel struct {
 	ID          int       `gorm:"primary_key;not null;AUTO_INCREMENT"`
 	Name        string    `gorm:"not null"`
 	Owner       string    `gorm:"not null"`
-	Destription string    `gorm:"not null"`
+	Description string    `gorm:"not null"`
 	CreatedAt   time.Time `gorm:"not null"`
 	ModifiedAt  time.Time `gorm:"not null"`
 }
