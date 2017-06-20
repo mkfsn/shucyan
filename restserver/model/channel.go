b4cc8ebcdb481dc26f73fb0df64cc0cf01442fee
mkfsn <p408865@gmail.com>
2017-06-20 22:09:37 +0800

    Update model to build relationship between channel, program, and tag

--- restserver/model/channel.go
+++ restserver/model/channel.go
@@ -5,9 +5,10 @@ import (
 )
 
 type Channel struct {
-	ID          int       `gorm:"primary_key;not null;AUTO_INCREMENT"`
+	ID          uint      `gorm:"primary_key;not null;AUTO_INCREMENT"`
 	Name        string    `gorm:"not null"`
 	Owner       string    `gorm:"not null"`
+	Programs    []Program `gorm:"not null;ForeignKey:ID"`
 	Description string    `gorm:"not null"`
 	CreatedAt   time.Time `gorm:"not null"`
 	ModifiedAt  time.Time `gorm:"not null"`
--- restserver/model/program.go
+++ restserver/model/program.go
@@ -2,17 +2,25 @@ package model
 
 import (
 	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 type Program struct {
-	ID         int       `gorm:"primary_key;not null;AUTO_INCREMENT"`
-	Day        int       `gorm:"not null"`
+	ID         uint      `gorm:"primary_key;not null;AUTO_INCREMENT"`
+	ChannelID  uint      `gorm:"uniq_key;not null"`
+	Day        uint      `gorm:"not null"`
 	Title      string    `gorm:"not null"`
 	Content    string    `gorm:"not null"`
 	Link       string    `gorm:"not null"`
-	Tags       string    `gorm:"not null"`
+	Tags       []Tag     `gorm:"many2many:program_tags;"`
 	StartedAt  time.Time `gorm:"not null"`
 	EndedAt    time.Time `gorm:"not null"`
 	CreatedAt  time.Time `gorm:"not null"`
 	ModifiedAt time.Time `gorm:"not null"`
+}
+
+type Tag struct {
+	gorm.Model
+	Name string
 }
