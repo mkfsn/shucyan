94357375f9fc999b8cfe687056c48def0fa1cdc8
mkfsn <p408865@gmail.com>
2017-06-20 08:11:55 +0800

    Add middleware to authenticate user

--- restserver/server.go
+++ restserver/server.go
@@ -6,9 +6,32 @@ import (
 	"github.com/mkfsn/shyuukan-program/restserver/model"
 )
 
+var secret = ""
+
+func middleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+
+		/* POST, PUT, DELETE need authentication */
+		if c.Request.Method != "GET" {
+			if cookie, err := c.Cookie("secret"); err != nil || cookie != secret {
+				// c.AbortWithStatus(403)
+				// return
+			}
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	db := model.InitDb("./model/database.sqlite3")
-	router := gin.Default()
+	router := gin.New()
+
+	router.Use(middleware())
 
 	ChannelController.Routes("/api/channels", router, db)
 
