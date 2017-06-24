7c229a82d5b2c6382708f2f9df8b717e3cb1baf3
mkfsn <p408865@gmail.com>
2017-06-24 13:39:51 +0800

    Use expressjs rather than http-server

---
+++ console/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+var path = __dirname;
+var port = 7000;
+
+function requestHandler(req, res) {
+    res.sendFile(path + '/index.html');
+}
+
+function requestForbidden(req, res) {
+    res.send(403);
+}
+
+app.use(express.static(path + '/dist'));
+
+app.get('/', requestHandler);
+app.get('/home', requestHandler);
+app.get('/login', requestHandler);
+app.get('/register', requestHandler);
+app.get('/about', requestHandler);
+app.get('/latest', requestHandler);
+app.get('/channel/*', requestHandler);
+
+app.get('/src/*', requestForbidden);
+app.get('/node_modules/*', requestForbidden);
+
+app.listen(port);
