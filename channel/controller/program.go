d342ce7ec2b5a5ded5f9a0e24dc0dd9d4f0410e7
mkfsn <p408865@gmail.com>
2017-07-02 12:01:58 +0800

    Clean program controller

--- channel/controller/program.go
+++ channel/controller/program.go
@@ -1,13 +1,10 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	"github.com/mkfsn/shyuukan-program/channel/model"
 )
 
 type ProgramController struct {
@@ -20,79 +17,15 @@ func NewProgramController(db *gorm.DB) Controller {
 	}
 }
 
-func (ctrl *ProgramController) getChannel(c *gin.Context) (model.Channel, error) {
-	var channel model.Channel
-
-	// FIXME: Is this ChannelID?
-	id, err := strconv.Atoi(c.PostForm("channelId"))
-	if err != nil || id < 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid channel id"})
-		return channel, fmt.Errorf("Invalid channel id")
-	}
-
-	if res := ctrl.db.First(&channel, id); res.RowsAffected == 0 {
-		c.JSON(http.StatusNotFound, gin.H{"message": "Channel id not found"})
-		return channel, fmt.Errorf("Channel id not found")
-	}
-
-	return channel, nil
-}
-
-func (ctrl *ProgramController) getProgramsByChannelID(c *gin.Context) {
-	var programs []model.Program
-
-	channel, err := ctrl.getChannel(c)
-	if err != nil {
-		return
-	}
-
-	ctrl.db.Model(&channel).Related(&programs)
-	c.JSON(http.StatusOK, programs)
-}
-
-func (ctrl *ProgramController) getProgramByID(c *gin.Context) {
-	var program model.Program
-
-	channel, err := ctrl.getChannel(c)
-	if err != nil {
-		return
-	}
-
-	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
-	if err != nil || id < 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid program id"})
-		return
-	}
-
-	ctrl.db.Where(&model.Program{ID: id, ChannelID: channel.ID}).First(&program)
-	c.JSON(http.StatusOK, program)
-}
-
-func (ctrl *ProgramController) delete(c *gin.Context) {
-	var program model.Program
-
-	cid, err := strconv.ParseUint(c.PostForm("channelId"), 10, 64)
-	if err != nil || cid < 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid channel id"})
-		return
-	}
-
-	id, err := strconv.ParseUint(c.Param("id"), 10, 64)
-	if err != nil || id < 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid program id"})
-		return
-	}
-
-	ctrl.db.Where(&model.Program{ID: id, ChannelID: cid}).First(&program)
-
-	c.JSON(http.StatusOK, program)
+func methodNotAllowed(context *gin.Context) {
+	context.AbortWithStatus(http.StatusMethodNotAllowed)
 }
 
 func (ctrl *ProgramController) AddRoutes(relativePath string, route *gin.Engine) {
 	programs := route.Group(relativePath)
 	{
-		programs.GET("/", ctrl.getProgramsByChannelID)
-		programs.GET("/:id", ctrl.getProgramByID)
-		programs.DELETE("/:id", ctrl.delete)
+		programs.GET("/", methodNotAllowed)
+		programs.GET("/:id", methodNotAllowed)
+		programs.DELETE("/:id", methodNotAllowed)
 	}
 }
