0423c67cda1a1810300df8bcc15bbbc693efdea6
mkfsn <p408865@gmail.com>
2017-07-14 23:40:49 +0800

    Refactored database: pass default channels to OpenDB

--- channel/model/database.go
+++ channel/model/database.go
@@ -1,10 +1,6 @@
 package model
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
-
 	"github.com/jinzhu/gorm"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,35 +9,23 @@ type Database struct {
 	db *gorm.DB
 }
 
-func NewDatabase(dbfile string, configFile string) *Database {
-	return &Database{
-		db: InitDB(dbfile, configFile),
-	}
-}
-
-func InitDB(dbfile string, configFile string) *gorm.DB {
+func OpenDB(dbfile string, debug bool, defaultChannels []Channel) *gorm.DB {
 	// Open database
 	db, err := gorm.Open("sqlite3", dbfile)
-	db.LogMode(true)
-
-	// Error
 	if err != nil {
 		panic(err)
 	}
 
-	file, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		log.Println(err)
-		return nil
+	db.LogMode(debug)
+
+	if debug {
+		initTables(db, defaultChannels)
 	}
 
-	var channels []Channel
-	err = json.Unmarshal(file, &channels)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
+	return db
+}
 
+func initTables(db *gorm.DB, channels []Channel) {
 	db.DropTableIfExists(&Program{}, &Channel{}, &Tag{})
 	db.AutoMigrate(&Program{}, &Channel{}, &Tag{})
 
@@ -53,6 +37,4 @@ func InitDB(dbfile string, configFile string) *gorm.DB {
 		db.Set("gorm:save_associations", false).Create(&channel)
 		db.Model(&channel).Association("Programs").Append(channel.Programs)
 	}
-
-	return db
 }
