dec2fafc21ffdd17a706f654b103f341f59e9283
mkfsn <p408865@gmail.com>
2017-07-14 23:42:04 +0800

    Refactored: remove flags except config file and use security middleware

--- channel/server.go
+++ channel/server.go
@@ -1,40 +1,34 @@
 package main
 
 import (
+	"flag"
+	"log"
+
+	channelConfig "github.com/mkfsn/shyuukan-program/channel/config"
 	"github.com/mkfsn/shyuukan-program/channel/controller"
+	"github.com/mkfsn/shyuukan-program/channel/security"
 
 	"github.com/gin-gonic/gin"
 	"github.com/mkfsn/shyuukan-program/channel/model"
 )
 
-var secret = ""
-
-func middleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-
-		/* POST, PUT, DELETE need authentication */
-		if c.Request.Method != "GET" {
-			if cookie, err := c.Cookie("secret"); err != nil || cookie != secret {
-				// c.AbortWithStatus(403)
-				// return
-			}
-		}
-
-		c.Next()
-	}
-}
-
 func main() {
 	var rest controller.Controller
 
-	db := model.InitDB("./model/database.sqlite3", "./default.json")
+	configFile := flag.String("config", "./config.yaml", "Config yaml file path")
+	flag.Parse()
+
+	config, err := channelConfig.ReadConfig(*configFile)
+	if err != nil {
+		log.Println("Cannot read config:", err)
+		config = channelConfig.DefaultConfig
+	}
+	log.Println("Config", config)
+
+	db := model.OpenDB(config.Server.Database, config.Server.Debug, config.Channels)
 	router := gin.New()
 
-	router.Use(middleware())
+	router.Use(security.Middleware(config))
 
 	rest = controller.NewChannelController(db)
 	rest.AddRoutes("/api/channels", router)
