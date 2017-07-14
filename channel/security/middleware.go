d277db8cd1ce48b8df2baff2c1628d1a57ef3dd6
mkfsn <p408865@gmail.com>
2017-07-15 00:30:38 +0800

    Add Access-Controls and ignore OPTIONS request

--- channel/security/middleware.go
+++ channel/security/middleware.go
@@ -8,14 +8,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	allowHeaders := "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"
+	allowMethods := "GET, POST, PUT, DELETE, OPTIONS"
+)
+
 func Middleware(config *config.Config) gin.HandlerFunc {
 
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", allowHeaders)
+		c.Writer.Header().Set("Access-Control-Allow-Methods", allowMethods)
 
 		/* POST, PUT, DELETE need authentication */
-		if c.Request.Method == "GET" {
+		if c.Request.Method == "GET" || c.Request.Method == "OPTIONS" {
 			c.Next()
 		}
 
