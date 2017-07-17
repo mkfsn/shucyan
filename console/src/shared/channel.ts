bf77ca692ec3c7b5ae99132cf655daa8651e5b19
mkfsn <p408865@gmail.com>
2017-07-17 09:00:39 +0800

    Remove unused function

--- console/src/shared/channel.ts
+++ console/src/shared/channel.ts
@@ -9,16 +9,6 @@ export class Channel {
     updatedAt: Date;
     programs: Array<Program>;
 
-    static fromJSON(data: Array<any>): Array<Channel> {
-        let channels: Array<Channel>;
-        return data.map((ch) => {
-            let programs: Array<Program> = ch.programs.map((pg) => {
-                return new Program(pg.day, pg.title, pg.content, pg.tags, pg.link, pg.startedAt);
-            });
-            return new Channel(ch.id, ch.title, ch.owner, ch.description, programs);
-        })
-    }
-
     constructor(id: string, title: string, owner: string, description?: string, programs?: Array<Program>, createdAt?: Date) {
         this.id = id;
         this.title = title;
