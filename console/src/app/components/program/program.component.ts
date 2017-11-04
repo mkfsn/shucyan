388aa7d1661fe133ff36b5a620b7db2af0102772
mkfsn <p408865@gmail.com>
2017-11-05 00:32:43 +0800

    fix: showing brighter tags

--- console/src/app/components/program/program.component.ts
+++ console/src/app/components/program/program.component.ts
@@ -68,11 +68,13 @@ export class ProgramComponent implements OnInit {
     // This should be protected by calling getColor()
     private calculateColor(tag: string): string {
         const sum = tag.split('').reduce((acc, val, arr) => acc * val.charCodeAt(0), 0x5F3759DF);
-        const itoa = (v: number): string => ('00' + Math.floor(v).toString(16)).substr(-2);
+        // NOTE: v / 2 + 128 for displaying brighter color
+        // e.g. 4 / 2 + 128 => 130, where 4 is too dark but 130 is not.
+        const itoa = (v: number): string => ('00' + Math.floor(v / 2 + 128).toString(16)).substr(-2);
         const b = itoa(sum % 256),
             g = itoa((sum / 256) % 256),
             r = itoa((sum / 256 / 256) % 256);
-        return chroma('#' + r + g + b).darken().hex();
+        return chroma('#' + r + g + b).saturate(2).hex();
     }
 
     private getColor(tag: string): string {
