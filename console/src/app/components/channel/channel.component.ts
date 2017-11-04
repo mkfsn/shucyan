ce768068f36956d5f731d79b63b55339ab8c214d
mkfsn <p408865@gmail.com>
2017-11-04 12:36:39 +0800

    feat: implement channel page for showing channel info

--- console/src/app/components/channel/channel.component.ts
+++ console/src/app/components/channel/channel.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, Params } from '@angular/router';
+
+import { ChannelsService } from '../../services/channels.service';
+
+import { Channel, NullChannel } from '../../models/channel';
+import { PageMode } from '../../models/page';
+
 @Component({
-  selector: 'app-channel',
-  templateUrl: './channel.component.html',
-  styleUrls: ['./channel.component.css']
+    selector: 'app-channel',
+    templateUrl: './channel.component.html',
+    styleUrls: ['./channel.component.scss']
 })
 export class ChannelComponent implements OnInit {
 
-  constructor() { }
+    private channel: Channel = NullChannel;
+    private mode: PageMode = new PageMode();
 
-  ngOnInit() {
-  }
+    constructor(private route: ActivatedRoute, private channelsService: ChannelsService) {
+        this.setModeByURL();
+        this.loadChannel();
+    }
+
+    ngOnInit() {
+    }
+
+    // Get channelId from URL
+    private getChannelId(): string {
+        return this.route.snapshot.params['id'];
+    }
+
+    private setChannel(channel: Channel): void {
+        this.channel = channel;
+        if (channel === NullChannel) {
+            return
+        }
+        // this.titleService.setTitle(this.channel.title);
+    }
+
+    private loadChannel(): void {
+        const channelId: string = this.getChannelId();
+
+        let successFunc = (channel) => {
+            this.setChannel(channel);
+        }
+
+        let errorFunc = () => {
+            this.setChannel(NullChannel);
+        }
+
+        this.channelsService.getChannels(channelId).subscribe(channels => {
+            if (channels.length > 0) {
+                successFunc(channels[0]);
+            } else {
+                errorFunc();
+            }
+        });
+    }
+
+    private setModeByURL(): void {
+        if (this.route.snapshot.url.length > 2 && this.route.snapshot.url[2].path === 'edit') {
+            this.mode.setEdit();
+        }
+        this.mode.setNormal();
+    }
+
+    get isEditMode(): boolean {
+        return this.mode.isEdit && this.channel !== undefined && this.channel !== NullChannel;
+    }
+
+    get isNormalMode(): boolean {
+        return this.mode.isNormal;
+    }
+
+    private nameInputLength(owner): string {
+        return `calc(100% - ${owner.length + 1}ch - 66px)`;
+    }
 
 }
