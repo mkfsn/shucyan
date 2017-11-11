1caa14cb4f7ae516800f6533e6bedaba8f61a3ba
mkfsn <p408865@gmail.com>
2017-11-11 12:02:40 +0800

    Add allowTo* methods to Channel

--- console/src/app/models/channel.ts
+++ console/src/app/models/channel.ts
@@ -1,4 +1,5 @@
 import { Program } from './program';
+import { User } from './user';
 
 type Email = string;
 
@@ -22,67 +23,89 @@ export class Channel {
     collaborators: ICollaborators;
     starred: Email[];
 
-    constructor(name: string, description: string) {
-        this.name = name;
-        this.description = description;
+    private user: User;
+
+    constructor(name: string, description: string, user?: User) {
+        this.id = null;
+        this.name = name || '';
+        this.owner = null;
+        this.description = description || '';
+        this.programs = [];
+        this.collaborators = {};
+        this.starred = [];
+
         if (name === undefined && description === undefined) {
             return;
         }
+
         this.createdAt = new Date();
-        this.starred = [];
-        this.collaborators = {};
+        this.updatedAt = null;
+
+        this.user = user || null;
     }
 
-    static fromFirebase(id: string, values: any): Channel {
-        const channel = new Channel(values.name, values.description);
+    static fromFirebase(id: string, values: any, user?: User): Channel {
+        const channel = new Channel(values.name, values.description, user);
         channel.id = id;
         channel.owner = values.owner;
         channel.collaborators = values.collaborators || {};
         channel.starred = values.starred || [];
         channel.createdAt = values.createdAt || null;
         channel.updatedAt = values.updatedAt || null;
-        console.log('fromFirebase:', channel);
+        console.debug('[fromFirebase]', channel);
         return channel;
     }
 
-    addCollaborators(...emails: string[]): void {
-        emails.forEach(email => {
-            email = this.emailEncode(email);
-            this.collaborators[email] = {
+    toFirebase(): Channel {
+        const channel = new Channel(this.name, this.description);
+        channel.id = null;
+        channel.owner = this.owner;
+        channel.createdAt = this.createdAt;
+        channel.updatedAt = this.updatedAt;
+        channel.programs = null;
+        channel.collaborators = this.collaborators;
+        channel.starred = this.starred;
+        console.debug('[toFirebase]', channel);
+        return channel;
+    }
+
+    get allowToEdit(): boolean {
+        return this.user && this.user.canEdit(this);
+    }
+
+    get allowToShare(): boolean {
+        return this.user && this.user.canShare(this);
+    }
+
+    get allowToStar(): boolean {
+        return this.user !== undefined && this.user !== null;
+    }
+
+    get allowToDelete(): boolean {
+        return this.user && this.user.canDelete(this);
+    }
+
+    addCollaborators(...users: User[]): void {
+        users.forEach(user => {
+            this.collaborators[user.encodedEmail] = {
                 editable: true
             };
         });
     }
 
-    removeCollaborators(...emails: string[]): void {
-        emails.forEach(email => {
-            email = this.emailEncode(email);
-            delete this.collaborators[email];
+    removeCollaborators(...users: User[]): void {
+        users.forEach(user => {
+            delete this.collaborators[user.encodedEmail];
         });
     }
 
-    isEditable(plainEmail: string) {
-        const collaborator = this.findCollaborator(plainEmail);
-        return (collaborator !== undefined && collaborator.editable) || this.owner === this.emailEncode(plainEmail);
+    belongsTo(user: User): boolean {
+        return user.encodedEmail === this.owner;
     }
 
-    findCollaborator(email: string): ICollaborator {
-        email = this.emailEncode(email);
-        return this.collaborators[email];
-    }
-
-    canShare(plainEmail: string): boolean {
-        return this.emailEncode(plainEmail) === this.owner;
-    }
-
-    canEdit(plainEmail: string): boolean {
-        return this.isEditable(plainEmail);
-    }
-
-    private emailEncode(plainEmail: string) {
-        // https://firebase.google.com/docs/reference/security/database/#replacesubstring_replacement
-        // https://groups.google.com/forum/#!topic/firebase-talk/vtX8lfxxShk
-        return plainEmail.replace('.', '%2E');
+    sharedWith(user: User): boolean {
+        const collaborator = this.collaborators[user.encodedEmail];
+        return collaborator && collaborator.editable;
     }
 
 }
