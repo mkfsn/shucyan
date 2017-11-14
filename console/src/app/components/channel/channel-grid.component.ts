a0d7a9e2be04975dc63f54f5003c692018d3e149
mkfsn <p408865@gmail.com>
2017-11-14 19:45:58 +0800

    (#22) styled channel grid layout

--- console/src/app/components/channel/channel-grid.component.ts
+++ console/src/app/components/channel/channel-grid.component.ts
@@ -1,23 +1,76 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, DoCheck, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Channel } from '../../models/channel';
+
+interface IChannel extends Channel {
+    selected: boolean;
+}
 
 @Component({
     selector: 'app-channel-grid',
     templateUrl: './channel-grid.component.html',
     styleUrls: ['./channel-grid.component.scss']
 })
-export class ChannelGridComponent implements OnInit {
+export class ChannelGridComponent implements OnInit, DoCheck {
 
     @Input('channels') private channels: Channel[];
 
     @Output('onRemoveChannel') onRemoveChannel = new EventEmitter();
 
-    constructor() { }
+    private selectableChannels: IChannel[];
+
+    constructor(private router: Router) { }
 
     ngOnInit() {}
 
+    ngDoCheck() {
+        if (this.channels) {
+            this.selectableChannels = this.channels.map((ch: Channel) => {
+                const channel = ch as IChannel;
+                const previousChannel = this.selectableChannels.find(c => c.id === ch.id);
+                if (previousChannel !== undefined) {
+                    channel.selected = previousChannel.selected;
+                } else {
+                    channel.selected = false;
+                }
+                return channel;
+            });
+        } else if (this.channels) {
+            this.selectableChannels = this.channels.map((ch: Channel) => {
+                const channel = ch as IChannel;
+                channel.selected = false;
+                return channel;
+            });
+        }
+    }
+
+    @HostListener('document:click', ['$event'])
+    private clickOutside(event) {
+        this.unselectAll();
+    }
+
+    private unselectAll(): void {
+        this.selectableChannels.forEach(ch => {
+            ch.selected = false;
+        });
+    }
+
+    private select(event, channelId: string): void {
+        this.unselectAll();
+        event.stopPropagation();
+
+        const channel = this.selectableChannels.find(ch => ch.id === channelId);
+        if (channel !== undefined) {
+            channel.selected = true;
+        }
+    }
+
     private removeChannel(channel: Channel): void {
         this.onRemoveChannel.emit(channel);
+    }
+
+    private navigateTo(channelId: string): void {
+        this.router.navigate(['/channel/' + channelId]);
     }
 }
